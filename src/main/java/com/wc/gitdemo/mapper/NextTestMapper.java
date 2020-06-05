1755ccdd1b9d462efb490b5adf6592d81bb33d39
wangchao149 <wangchao149@hellobike.com>
2020-06-05 13:55:33 +0800

    nextTest

---
+++ src/main/java/com/wc/gitdemo/mapper/NextTestMapper.java
@@ -0,0 +1,17 @@
+/**
+ * NextTestMapper.java
+ * Copyright 2020 HelloBike , all rights reserved.
+ * HelloBike PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
+ */
+
+package com.wc.gitdemo.mapper;
+
+/**
+ * @author 86188
+ * @date 2020/6/5
+ */
+public class NextTestMapper {
+    public void nextTest(){
+        System.out.println("再次测试。。。。");
+    }
+}
