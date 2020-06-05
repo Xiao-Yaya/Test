28f26ff519bac041589735a8bdf17dbfc5bd5032
wangchao149 <wangchao149@hellobike.com>
2020-06-05 10:25:56 +0800

    hello

---
+++ src/main/java/com/wc/gitdemo/mapper/HelloMapper.java
@@ -0,0 +1,17 @@
+/**
+ * HelloMapper.java
+ * Copyright 2020 HelloBike , all rights reserved.
+ * HelloBike PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
+ */
+
+package com.wc.gitdemo.mapper;
+
+/**
+ * @author 86188
+ * @date 2020/6/5
+ */
+public class HelloMapper {
+    public void hello(){
+        System.out.println("hihihihihihi");
+    }
+}
