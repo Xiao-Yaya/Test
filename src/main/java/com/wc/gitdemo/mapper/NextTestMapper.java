5e7344c88aa9ae2d5938678b75de9684077ddac6
wangchao149 <wangchao149@hellobike.com>
2020-06-05 14:02:38 +0800

    nextTest

--- src/main/java/com/wc/gitdemo/mapper/NextTestMapper.java
+++ src/main/java/com/wc/gitdemo/mapper/NextTestMapper.java
@@ -12,6 +12,6 @@ package com.wc.gitdemo.mapper;
  */
 public class NextTestMapper {
     public void nextTest(){
-        System.out.println("再次测试。。。。");
+        System.out.println("再次测试。。。");
     }
 }
